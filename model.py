1e0c0b34178ad3784533d5a581b3d92c31527941
Peyman Baghershahi <peyman.baghershahi@gmail.com>
2023-11-10 11:38:30 -0600

    pretraining added

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
-import torch
+import torch, os
 import torch.nn as nn
 import torch.nn.functional as F
+from datetime import datetime
 from torch.optim import Adam
 from torch.optim.lr_scheduler import StepLR
 import numpy as np
@@ -9,15 +10,11 @@ import pandas as pd
 from typing import List
 from torch_geometric.data import Data
 from torch_geometric.utils import augmentation
-from torch_geometric.nn import GAT, GATConv
+from torch_geometric.nn import GATConv
 from torch_geometric.datasets import QM9, TUDataset
 from torch_geometric.loader import DataLoader
 from torch_geometric.nn import global_mean_pool
 
-def drop_edges(org_graph, n_drops):
-    perm = torch.randperm(org_graph.edge_index.size(1))[n_drops:]
-    org_graph.edge_index = org_graph.edge_index[:, perm]
-    return org_graph
 
 class PromptGraph(nn.Module):
     def __init__(self, num_pnodes, emb_dim) -> None:
@@ -43,24 +40,31 @@ class PromptGraph(nn.Module):
         return graphs
 
 class GNNGraphClass(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim=None, num_layers=1, normalize=True) -> None:
+    def __init__(self, input_dim, hidden_dim, output_dim=None, num_layers=1, normalize=True, has_head=True) -> None:
         super(GNNGraphClass, self).__init__()
         output_dim = hidden_dim if output_dim is None else output_dim
         in_dims = [input_dim]+[hidden_dim]*(num_layers-1)
         out_dims = [hidden_dim]*(num_layers)
         inner_layers = [GATConv(in_dim, out_dim) for (in_dim, out_dim) in zip(in_dims, out_dims)]
+        self.normalize = normalize
         if normalize:
             inner_layers = [inner_layers[i//2] if i%2==0 else nn.BatchNorm1d(out_dims[i//2]) for i in range(num_layers*2)]
         self.gnn_layers = nn.ModuleList(inner_layers)
-        self.head = nn.Linear(hidden_dim, output_dim)
+        self.has_head = has_head
+        if has_head and output_dim is not None:
+            self.head = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, graph_x, edge_index, batch):
         emb_out = graph_x.squeeze().to(torch.float32)
-        for i in range(0, len(self.gnn_layers), 2):
+        i = 0
+        while i < len(self.gnn_layers):
             emb_out = self.gnn_layers[i](emb_out, edge_index)
-            emb_out = F.relu(self.gnn_layers[i+1](emb_out))
+            if self.normalize:
+                i += 1
+                emb_out = F.relu(self.gnn_layers[i](emb_out))
+            i += 1
         emb_out = global_mean_pool(emb_out, batch)
         emb_out = F.dropout(emb_out, p=0.5, training=self.training)
-        emb_out = self.head(emb_out)
-        # emb_out = F.sigmoid(emb_out)
+        if self.has_head:
+            emb_out = self.head(emb_out)
         return emb_out
---
+++ utils.py
@@ -0,0 +1,34 @@
+import torch, os
+import torch.nn as nn
+import torch.nn.functional as F
+from datetime import datetime
+from torch.optim import Adam
+from torch.optim.lr_scheduler import StepLR
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from typing import List
+
+def test(loader, model):
+    model.eval()
+    correct = 0
+    for data in loader:
+        out = model(data.x, data.edge_index, data.batch)
+        pred = out.max(dim=1)[1]
+        correct += int((pred == data.y).sum())
+    return correct / len(loader.dataset)
+
+def drop_edges(org_graph, drop_prob):
+    n_edges = org_graph.edge_index.size(1)
+    perm = torch.randperm(n_edges)[int(n_edges * n_drops):]
+    org_graph.edge_index = org_graph.edge_index[:, perm]
+    return org_graph
+
+def load_model_(self, cmodel, pmodel, read_checkpoint=True, pretrained_path=None):
+    if read_checkpoint and pretrained_path is not None:
+        pretrained_dict = torch.load(pretrained_path)
+    else:
+        pretrained_dict = pmodel.state_dict()
+    model_dict = cmodel.state_dict()
+    pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+    cmodel.load_state_dict(pretrained_dict)
