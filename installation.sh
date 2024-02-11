d47142eff9ead10e30af60d656f857700697e4e3
Peyman <p.baghershahi@gmail.com>
2024-02-11 18:08:30 +0000

    installation updated

--- installation.sh
+++ installation.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-pytorch_version=$(python -c "import torch; print(f'torch-{torch.__version__}.html')")
-pip install --no-index pyg_lib -f https://pytorch-geometric.com/whl/$pytorch_version
-pip install --no-index torch-scatter -f https://pytorch-geometric.com/whl/$pytorch_version
-pip install --no-index torch-sparse -f https://pytorch-geometric.com/whl/$pytorch_version
-pip install --no-index torch-cluster -f https://pytorch-geometric.com/whl/$pytorch_version
-pip install --no-index torch-spline-conv -f https://pytorch-geometric.com/whl/$pytorch_version
+curr_version=$(python -c "import torch; print(torch.__version__)")
+pytorch_version="${1:-$curr_version}"
+pytorch_version="torch-${pytorch_version}.html"
+pip install --no-index pyg_lib==0.3.1 torch-scatter torch-sparse \
+torch-cluster torch-spline-conv -f https://pytorch-geometric.com/whl/$pytorch_version
 pip install torch-geometric
-pip install osmnx
-python -c "from IPython.display import clear_output; clear_output(); print('Installed requirements')"
+printf "\033c"
